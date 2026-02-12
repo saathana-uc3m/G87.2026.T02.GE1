b174cfa8994e03547eefd6dfd7b53783aee5b284
UC3M-GE1-a <100589384@alumnos.uc3m.es>
2026-02-12 17:53:24 +0100

    Enterprise Management Excepion pylint fixes

--- UC3MConsulting/EnterpriseManagementException.py
+++ UC3MConsulting/EnterpriseManagementException.py
@@ -1,13 +1,17 @@
+'''Enterprise Management Exception class'''
+
 class EnterpriseManagementException(Exception):
+    '''Exception class for Enterprise Management Exception'''
     def __init__(self, message):
         self.__message = message
         super().__init__(self.message)
 
     @property
     def message(self):
+        '''get enterprise message'''
         return self.__message
 
     @message.setter
     def message(self,value):
+        '''set enterprise message'''
         self.__message = value
-
